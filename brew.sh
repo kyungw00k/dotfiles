9427488980eba51d2d6e109d994a6ed7fde0b97a
Kyungwook Park <parksama@gmail.com>
2017-10-10 13:56:12 +0900

    add brew related script

--- brew.sh
+++ brew.sh
@@ -53,32 +53,5 @@ brew install asciinema
 # Install Cask
 brew install caskroom/cask/brew-cask
 
-# In order to make your Casks install to sensible and predictable locations,
-# recommend adding the following line
-export HOMEBREW_CASK_OPTS="--appdir=/Applications"
-
-# Install Casks
-brew cask install alfred
-brew cask install virtualbox
-brew cask install vagrant
-brew cask install iterm2
-brew cask install google-chrome
-brew cask install 1password
-brew cask install things
-brew cask install keepingyouawake
-brew cask install charles
-brew cask install the-unarchiver
-brew cask install flux
-brew cask install noizio
-brew cask install noun-project
-
-# Image Upload to imgur.com
-brew cask install mac2imgur
-
-# Track time in websites and applications
-brew cask install rescuetime
-
-# brew cask install genymotion
-
 # Remove outdated versions from the cellar
 brew cleanup
