aba138047939dead58389a531650a0aa9e8eb06f
Kyungwook Park <parksama@gmail.com>
2017-12-09 01:21:38 +0900

    fix JAVA_HOME

--- java/path.zsh
+++ java/path.zsh
@@ -1,4 +1,4 @@
 # java
-export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
+export JAVA_HOME=`/usr/libexec/java_home`
 export JAVA_TOOL_OPTIONS=" -Dfile.encoding=UTF8"
 export PATH=$JAVA_HOME/bin:$PATH
