c6dd524e528a295d2e819f36b0141d0e3f2f5007
Kyungwook Park <parksama@gmail.com>
2019-05-22 23:08:23 +0900

    ZSH_CUSTOM 변수 관련 설정 변경

--- zsh/install.sh
+++ zsh/install.sh
@@ -4,5 +4,5 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # zsh-autosuggestions
-git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone git://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
